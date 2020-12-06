d9e53353e0fb196b82921011df0743dfa45a5b81
tiago <you@example.com>
2020-12-06 16:01:08 +0000

    wip

--- TP/grafos.c
+++ TP/grafos.c
@@ -2,8 +2,10 @@
 #include <stdlib.h>
 
 #define NE 0
-
 #define MAX 100
+#define WHITE 0
+#define GRAY 1
+#define BLACK 2
 
 typedef int WEIGHT;
 
@@ -16,6 +18,43 @@ struct edge {
 typedef struct edge *GraphL[MAX];
 
 typedef WEIGHT GraphM[MAX][MAX];
+
+typedef struct queue {
+int inicio, tamanho;
+int valores [MAX];
+} QUEUE;
+
+
+int color[MAX];
+int parent[MAX];
+
+void initQueue (QUEUE *q)
+{
+    q->inicio = 0;
+    q->tamanho = 0;
+}
+
+int isEmptyQ (QUEUE *q)
+{
+    return q->tamanho == 0;
+}
+
+void enqueue (QUEUE *q, int x)
+{
+    if (q->tamanho < MAX)
+    {
+        q->valores[(q->inicio + q->tamanho) % MAX] = x;
+        q->tamanho++; 
+    }
+}
+
+int dequeue (QUEUE *q)
+{
+    int r = q->valores[q->inicio];
+    q->tamanho--;
+    q->inicio = (q->inicio + 1) % MAX; 
+    return r;
+}
 
 void printGraphM(GraphM g, int N)
 {
@@ -91,7 +130,7 @@ void graphMtoL(GraphL gl, GraphM gm, int N)
             if (gm[i][j] != 0)
             {
                 *ptr = malloc (sizeof (struct edge));
-                (*ptr)->dest = j+1;
+                (*ptr)->dest = j;
                 (*ptr)->weight = gm[i][j];
                 ptr = &((*ptr)->next); 
             }      
@@ -103,7 +142,7 @@ int inDegreeM (GraphM gm, int N, int v)
 {
     int i, r = 0;
     for(i=0; i<N; i++)
-        if (gm[i][v-1] != 0)
+        if (gm[i][v] != 0)
             r++;
     return r;
 }
@@ -112,7 +151,7 @@ int outDegreeM (GraphM gm, int v)
 {
     int i, r = 0;
     for(i=0; i<MAX; i++)
-        if (gm[v-1][i] != 0)
+        if (gm[v][i] != 0)
             r++;
     return r;
 
@@ -135,7 +174,7 @@ int outDegreeL (GraphL gl, int v)
 {
     int r = 0;
     struct edge * aux = NULL;
-    aux = gl[v-1];
+    aux = gl[v];
     while (aux)
     {
         r++;
@@ -144,3 +183,137 @@ int outDegreeL (GraphL gl, int v)
     return r;
 }
 
+void printDegreeL (GraphL gl, int N) // imprme graus de entrada e saida dos vertices do grafo
+{
+    int i, inDeg, outDeg;
+
+    for(i=0; i<N; i++)
+    {
+        inDeg = inDegreeL(gl,N,i);
+        outDeg = outDegreeL(gl,i);
+        printf("Verice %d :: Grau Entrada %d :: Grau Saida %d\n",i, inDeg, outDeg);
+    }
+}
+
+
+void df_visit(GraphL g, int s)
+{
+    color[s] = GRAY;
+    printf ("%d GRAY\n", s);
+    struct edge * aux = g[s];
+    while (aux)
+    {
+        if(color[aux->dest] == WHITE)
+        {
+            parent[aux->dest] = s;
+            df_visit (g, aux->dest);
+        }
+        aux = aux->next; 
+    }
+    color[s] = BLACK;
+    printf ("%d BLACK\n", s);
+
+}
+
+
+void dfs (GraphL g, int n)
+{
+    int i;
+    for(i=0; i<n; i++)
+    {
+        color[i] = WHITE;
+        parent[i] = -1;
+    }
+
+    for(i=0; i<n; i++)
+        if (color[i] == WHITE)
+            df_visit(g,i);
+}
+
+void bf_visit(GraphL g, int s)
+{
+    QUEUE q;
+    initQueue (&q);
+    color[s] = GRAY;
+    printf ("%d GRAY\n", s);
+    parent[s] = 0;
+    enqueue(&q, s);
+    while (! isEmptyQ (&q))
+    {
+        int u = dequeue (&q);
+        struct edge * aux = g[u];
+        while (aux)
+        {
+            if (color[aux->dest] == WHITE)
+            {
+                color[aux->dest] = GRAY;
+                printf ("%d GRAY\n", aux->dest);
+                parent[aux->dest] = u;
+                enqueue (&q, aux->dest);
+            }
+            aux = aux->next;
+        }
+        color[u] = BLACK;
+        printf ("%d BLACK\n", u);
+    }
+
+
+    
+}
+
+void bfs (GraphL g, int n)
+{
+    int i;
+    for(i=0; i<n; i++)
+    {
+        color[i] = WHITE;
+        parent[i] = -1;
+    }
+
+    for(i=0; i<n; i++)
+        if (color[i] == WHITE)
+            bf_visit(g,i);
+    
+}
+
+
+int main ()
+{
+    GraphM gm3 = {
+    {0, 1, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 1, 0, 0, 0, 1, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 1, 0},
+    {0, 0, 1, 0, 1, 0, 0, 1, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {1, 0, 0, 0, 1, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 1, 0, 1},
+    {0, 0, 0, 0, 1, 0, 1, 0, 0},
+    }; 
+    
+    //printGraphM (gm3, n3);
+    //putchar('\n');
+
+    GraphL gl3;
+    int n3 = 9;
+    graphMtoL(gl3, gm3, n3);
+
+    //printGraphL(gl3, n3);
+    //putchar('\n');
+   
+    //printDegreeL (gl3, n3);
+    //putchar('\n');
+
+    printf("Depth-first traversal:\n");
+    dfs(gl3, n3);
+    putchar('\n');
+
+
+    printf("Breadth-first traversal:\n");
+    bfs(gl3, n3);
+    putchar('\n');
+
+
+    return 0;
+     
+}
