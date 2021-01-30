6d6ca5bf65850792c0e7d7e408f7fccb60e0101d
zoisper <zoisper@gmail.com>
2021-01-30 18:21:51 +0000

    wip

--- TP/Grafos.c
+++ TP/Grafos.c
@@ -1,67 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define NE 0
 #define MAX 100
 #define WHITE 0
 #define GRAY 1
 #define BLACK 2
 
-typedef int WEIGHT;
-
 struct edge {
-  int dest;
-  WEIGHT weight;
-  struct edge *next;
+int dest;
+int weight;
+struct edge *next;
 };
 
 typedef struct edge *GraphL[MAX];
 
-typedef WEIGHT GraphM[MAX][MAX];
-
-typedef struct queue {
-int inicio, tamanho;
-int valores [MAX];
-} QUEUE;
+typedef int GraphM[MAX][MAX];
 
 
-int color[MAX];
-int parent[MAX];
-
-void initQueue (QUEUE *q)
-{
-    q->inicio = 0;
-    q->tamanho = 0;
-}
-
-int isEmptyQ (QUEUE q)
-{
-    return q.tamanho == 0;
-}
-
-void enqueue (QUEUE *q, int x)
-{
-    if (q->tamanho < MAX)
-    {
-        q->valores[(q->inicio + q->tamanho) % MAX] = x;
-        q->tamanho++; 
-    }
-}
-
-int dequeue (QUEUE *q)
-{
-    int r = q->valores[q->inicio];
-    q->tamanho--;
-    q->inicio = (q->inicio + 1) % MAX; 
-    return r;
-}
-
-void printGraphM(GraphM g, int N)
+void printGraphM(GraphM g, int size)
 {
     int i, j;
-    for(i=0; i<N; i++)
+    for(i=0; i<size; i++)
     {
-        for(j=0; j<N; j++)
+        for(j=0; j<size; j++)
         if (g[i][j] != 0)
             printf ("1 ");
         else
@@ -71,61 +32,52 @@ void printGraphM(GraphM g, int N)
     }
 }
 
-void printGraphM_Weight(GraphM g, int N) // imprime vertices com peso
+void printGraphM_Weight(GraphM g, int size) // imprime vertices com peso
 {
     int i, j;
-    for(i=0; i<N; i++)
+    for(i=0; i<size; i++)
     {
-        for(j=0; j<N; j++)
+        for(j=0; j<size; j++)
             printf ("%d ",g[i][j]);
         putchar('\n');
     }
 }
 
-void printGraphL(GraphL gl, int N)
+void printGraphL(GraphL gl, int size)
 {
     int i;
     struct edge * aux = NULL;
-    for(i=0; i<N; i++)
+    for(i=0; i<size; i++)
     {
-        aux = gl[i];
-        while(aux)
-        {
+        for (aux = gl[i]; aux; aux = aux->next)
             printf ("%d ",aux->dest);
-            aux = aux->next;
-        }
+            
         putchar('\n');
     }
 }
 
-void graphLtoM(GraphM gm, GraphL gl, int N)
+void graphLtoM(GraphM gm, GraphL gl, int size)
 {
     int i, j;
     struct edge * aux = NULL;
-    for(i=0; i<N; i++)
-        for(j=0; j<N; j++)
+    
+    for(i=0; i<size; i++)
+        for(j=0; j<size; j++)
             gm[i][j] = 0;
 
-    for(i=0; i<N; i++)
-    {
-        aux = gl[i];
-        while(aux)
-        {
+    for(i=0; i<size; i++)
+        for (aux = gl[i]; aux; aux = aux->next)
             gm[i][aux->dest] = aux->weight;
-            aux = aux->next;
-        }
-    }
-        
-    
 }
-void graphMtoL(GraphL gl, GraphM gm, int N)
+
+void graphMtoL(GraphL gl, GraphM gm, int size)
 {
     int i, j;
     struct edge **ptr = NULL;
-    for(i=0; i<N; i++)
+    for(i=0; i<size; i++)
     {
         ptr = &(gl[i]);
-        for(j=0; j<N; j++)
+        for(j=0; j<size; j++)
         {
             if (gm[i][j] != 0)
             {
@@ -138,31 +90,32 @@ void graphMtoL(GraphL gl, GraphM gm, int N)
     }
 }
 
-int inDegreeM (GraphM gm, int N, int v)
+int inDegreeM (GraphM g, int size, int v)
 {
     int i, r = 0;
-    for(i=0; i<N; i++)
-        if (gm[i][v] != 0)
+    for(i=0; i<size; i++)
+        if (g[i][v] != 0)
             r++;
     return r;
 }
 
-int outDegreeM (GraphM gm, int v)
+int outDegreeM (GraphM g, int size,  int v)
 {
     int i, r = 0;
-    for(i=0; i<MAX; i++)
-        if (gm[v][i] != 0)
+    for(i=0; i<size; i++)
+        if (g[v][i] != 0)
             r++;
     return r;
 
 }
-int inDegreeL (GraphL gl, int N, int v)
+
+int inDegreeL (GraphL g, int size, int v)
 {
     int i, r = 0;
     struct edge * aux = NULL;
-    for(i=0; i<N; i++)
+    for(i=0; i<size; i++)
     {
-        aux = gl[i];
+        aux = g[i];
         while (aux && aux->dest < v)
             aux = aux->next;
         if (aux && aux->dest == v)
@@ -183,160 +136,224 @@ int outDegreeL (GraphL gl, int v)
     return r;
 }
 
-void printDegreeL (GraphL gl, int N) // imprme graus de entrada e saida dos vertices do grafo
+void printDegreeL (GraphL g, int size) // imprme graus de entrada e saida dos vertices do grafo
 {
     int i, inDeg, outDeg;
 
-    for(i=0; i<N; i++)
+    for(i=0; i<size; i++)
     {
-        inDeg = inDegreeL(gl,N,i);
-        outDeg = outDegreeL(gl,i);
+        inDeg = inDegreeL(g,size,i);
+        outDeg = outDegreeL(g,i);
         printf("Verice %d :: Grau Entrada %d :: Grau Saida %d\n",i, inDeg, outDeg);
     }
 }
 
-
-void df_visit(GraphL g, int s)
+void df_visit(GraphL g, int v, int color[], int parent[]) // travessia em profundidade partindo do verice v
 {
-    color[s] = GRAY;
-    printf ("%d GRAY\n", s);
-    struct edge * aux = g[s];
-    while (aux)
-    {
+    color[v] = GRAY;
+    printf ("%d GRAY\n", v);
+    struct edge * aux; 
+    for (aux = g[v]; aux; aux = aux->next )
         if(color[aux->dest] == WHITE)
         {
-            parent[aux->dest] = s;
-            df_visit (g, aux->dest);
+            parent[aux->dest] = v;
+            df_visit (g, aux->dest, color, parent);
         }
-        aux = aux->next; 
-    }
-    color[s] = BLACK;
-    printf ("%d BLACK\n", s);
+
+    color[v] = BLACK;
+    printf ("%d BLACK\n", v);
 
 }
 
-
-void dfs (GraphL g, int n)
+void dfs (GraphL g, int size) // função de topo que faz travessia em profundidade apartir de todos os vertices ainda não visitados
 {
-    int i;
-    for(i=0; i<n; i++)
+    int i, color[size], parent[size];
+    for(i=0; i<size; i++)
     {
         color[i] = WHITE;
         parent[i] = -1;
     }
 
-    for(i=0; i<n; i++)
+    for(i=0; i<size; i++)
         if (color[i] == WHITE)
-            df_visit(g,i);
+            df_visit(g,i, color, parent);
 }
 
-void bf_visit(GraphL g, int s)
+void bf_visit(GraphL g, int v, int color[], int parent[]) // travessia em largura partindo do verice v
 {
-    QUEUE q;
-    initQueue (&q);
-    color[s] = GRAY;
-    printf ("%d GRAY\n", s);
-    parent[s] = 0;
-    enqueue(&q, s);
-    while (! isEmptyQ (q))
+    int u, firstF = 0, lastF = 0, fringe[MAX];
+    struct edge * aux;
+
+    color[v] = GRAY;
+    printf ("%d GRAY\n", v);
+    fringe [lastF++] = v;
+    while (lastF > firstF)
     {
-        int u = dequeue (&q);
-        struct edge * aux = g[u];
-        while (aux)
-        {
+        u = fringe[firstF++];
+        for (aux = g[u]; aux; aux = aux->next)
             if (color[aux->dest] == WHITE)
             {
                 color[aux->dest] = GRAY;
-                printf ("%d GRAY\n", aux->dest);
                 parent[aux->dest] = u;
-                enqueue (&q, aux->dest);
+                fringe [lastF++] = aux->dest;
+                printf ("%d GRAY\n", aux->dest);
             }
-            aux = aux->next;
-        }
+            
         color[u] = BLACK;
         printf ("%d BLACK\n", u);
     }
-
-
-    
 }
 
-void bfs (GraphL g, int n)
+void bfs (GraphL g, int size) // função de topo que faz travessia em largura apartir de todos os vertices ainda não visitados
 {
-    int i;
-    for(i=0; i<n; i++)
+    int i, color[size], parent[size];
+    for(i=0; i<size; i++)
     {
         color[i] = WHITE;
         parent[i] = -1;
     }
 
-    for(i=0; i<n; i++)
+    for(i=0; i<size; i++)
         if (color[i] == WHITE)
-            bf_visit(g,i);
+            bf_visit(g,i, color, parent);
+}
+
+
+int minEdge (int color[], int dist[], int size) // calcula qual a aresta com menor peso
+{
+    int i, r;
+    for(i=0; color[i] != GRAY; i++); 
+    r = i; // encontrar um valor inicial para o r
     
+    for(; i<size; i++)
+        if (color[i] == GRAY && dist[i] < dist[r])
+            r = i;
+    return r;   
 }
 
-int calcDist (int orig, int dest)
+void calcPaths(GraphL g, int size, int source, int parent[]) // calcula caminhos menores partindo de v
 {
-    if (orig == dest)
-        return 0;
-    else
-        if (parent[dest] >=0)
-            return 1 + calcDist (orig, parent[dest]);
-    else
-        return -1;
-}
+    int i, fringe = 1, dist[size], color[size];
+    struct edge * aux;
 
-void dists (GraphL g, int s, int dist[]) 
-{
-    int i;
-    for(i=0; i<MAX; i++)
+    for(i=0; i<size; i++)
     {
         color[i] = WHITE;
-        parent[i] = -1;
+        parent[i] = -2;
     }
-    QUEUE q;
-    initQueue (&q);
-    color[s] = GRAY;
-    enqueue(&q, s);
-    while (! isEmptyQ (q))
+
+    color[source] = GRAY;
+    parent[source] = -1;
+    dist[source] = 0;
+    
+    while (fringe > 0)
     {
-        int u = dequeue (&q);
-        struct edge * aux = g[u];
-        while (aux)
-        {
+        fringe--;
+        for (aux = g[source]; aux; aux = aux->next)
             if (color[aux->dest] == WHITE)
             {
                 color[aux->dest] = GRAY;
-                parent[aux->dest] = u;
-                enqueue (&q, aux->dest);
+                parent[aux->dest] = source;
+                dist[aux->dest] = dist[source] + aux->weight;
+                fringe++;
             }
-            aux = aux->next;
-        }
-        color[u] = BLACK;
+            else    
+                if (color[aux->dest] == GRAY && dist[aux->dest] > dist[source] + aux->weight)
+                {
+                    dist[aux->dest] = dist[source] + aux->weight;
+                    parent[aux->dest] = source;
+                }
+        color[source] = BLACK;
+        source = minEdge(color, dist, size);
     }
-    for (i=0; i<MAX; i++)
-        dist[i] = calcDist (s, i);
 }
 
-void shortestPath (GraphL g, int s, int  d) 
-{
-    int dist[MAX];
-    dists(g, s, dist);
-    int i;
-    if (dist[d] == -1)
-        printf ("%d nao alcancavel a partir de %d", d, s);
-    else
-    {
-        printf("Caminho inverso de %d para %d:\n", s, d);
-        while (s != d)
-        {
-            printf ("%d ", d);
-            d = parent[d];
-        }
-        printf("%d ",s );
 
+void showPathAux (GraphL g, int source, int destiny, int parent[])
+{
+    
+    if(source != destiny)
+    {
+        showPathAux (g, source, parent[destiny], parent);
+        printf ("%d ", destiny);
     }
+    
+    else
+        printf("%d ",source );       
+}
+
+void showPath (GraphL g, int source, int destiny, int parent[]) // Imprime caminho 
+{
+    if (parent[destiny] == -2)
+            printf ("%d nao alcancavel a partir de %d\n", destiny, source);
+        else
+        {
+            printf("Caminho de %d para %d:\n", source, destiny);
+            showPathAux (g, source, destiny, parent);
+        }
+}
+
+void showPathReverse (GraphL g, int source, int destiny, int parent[]) // Imprime caminho ao contrario
+{
+    if (parent[destiny] == -2)
+            printf ("%d nao alcancavel a partir de %d\n", destiny, source);
+        else
+        {
+            printf("Caminho inverso de %d para %d:\n", source, destiny);
+            while (source != destiny)
+            {
+                printf ("%d ", destiny);
+                destiny = parent[destiny];
+            }
+            printf("%d ",source );
+        }
+} 
+
+
+void shortestPath (GraphL g, int size, int source, int destiny)
+{
+    int parent[size];
+    calcPaths(g, size, source, parent);
+    showPath(g, source, destiny, parent);
+    putchar('\n');
+    showPathReverse(g, source, destiny, parent);
+
+}
+
+
+
+
+
+
+int main ()
+{
+
+    GraphM gm1 = {
+    {0, 1, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 1, 0, 0, 0, 1, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 1, 0},
+    {0, 0, 1, 0, 1, 0, 0, 1, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {1, 0, 0, 0, 1, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 1, 0, 1},
+    {0, 0, 0, 0, 1, 0, 1, 0, 0},
+    }; 
+
+    GraphL gl;
+    graphMtoL (gl, gm1, 9 ); 
+    //dfs(gl, 9);
+    //printf ("\n---------------------------------\n");
+    //bfs(gl, 9);
+    //printf ("\n---------------------------------\n");
+    shortestPath(gl, 9,  3, 5);
+    printf ("\n---------------------------------\n");
+    shortestPath(gl, 9, 3, 1);
+
+
+
+
+    return 0;
 }
 
 
